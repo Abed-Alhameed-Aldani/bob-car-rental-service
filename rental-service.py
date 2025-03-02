4ab9debc0e5e399cac09c788275ad6e8f7aff4d4
Abed-Aldani <abedaldani2@gmail.com>
2025-03-02 22:11:52 +0200

    Adding another Method for setting rental prices and displaying them

--- rental-service.py
+++ rental-service.py
@@ -6,4 +6,10 @@ class vehicle :
         self.__rental_price_per_day=rental_price_per_day
     def get_rental_price_per_day(self):
         return self.__rental_price_per_day
-    
+    def set_rental_price_per_day(self,newPrice):
+        self.__rental_price_per_day=newPrice
+    def display_info(self):
+       return(
+            f"{self.__class__.__name__}:{self.brand} {self.model},Year:{self.year},"
+            f"Renatal Price:${self.__rental_price_per_day}/day"
+        )
