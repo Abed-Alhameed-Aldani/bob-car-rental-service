8cd3ce48ec82a3f168f5999c13c315853c28cb19
Abed-Aldani <abedaldani2@gmail.com>
2025-03-02 22:10:17 +0200

    Adding another Method for rental prices

--- rental-service.py
+++ rental-service.py
@@ -4,4 +4,6 @@ class vehicle :
         self.model=model
         self.year=year
         self.__rental_price_per_day=rental_price_per_day
+    def get_rental_price_per_day(self):
+        return self.__rental_price_per_day
     
